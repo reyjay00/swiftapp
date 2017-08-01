ca170bb0b6eeee22440f1f7643a333e0cc114997
Joel Reynolds <reynolds.joel@gmail.co>
2017-08-01 11:55:02 +1000

    First commit

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    var tapcount = 0
+    var firstnum = 0
+    var secondnum = 0
+    var thirdnum = 0
+    
+    @IBAction func Num1(_ sender: Any) {
+    }
+    
+    @IBAction func Num2(_ sender: Any) {
+    }
+    
+    @IBAction func Num3(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Button(_ sender: Any) {
+        TheLabel.text = "Hello There!"
+        // print("Hello CLicked")
+         tapcount = tapcount + 1
+        print ("Tap Count =", tapcount)
+        if tapcount > 5 {
+            TheLabel.text = "You've pressed > 10"
+        }
+
+        
+    }
+    
+    
+    @IBAction func PushMe(_ sender: Any) {
+        TheLabel.text = "Original"
+        // print("Hello CLicked")
+        tapcount = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+        
     }
 
     override func didReceiveMemoryWarning() {
